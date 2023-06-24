9ddcd779cce37a52185da161ac67464c98957112
tmasuyama1114 <tmasuyama1114@gmail.com>
2023-06-25 06:58:17 +0900

    Simplify blog section title text

--- app/page.js
+++ app/page.js
@@ -31,12 +31,7 @@ export default function Blogs() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            From the blog
-          </h2>
-          <p className="mt-2 text-lg leading-8 text-gray-600">
-            Learn how to grow your business with our expert advice.
-          </p>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Blog</h2>
           <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
             {posts.map((post) => (
               <article
