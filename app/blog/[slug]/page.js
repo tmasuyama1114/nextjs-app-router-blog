f3585ac7c784bc34428289a53e0a06edc0607053
tmasuyama1114 <tmasuyama1114@gmail.com>
2023-06-24 20:42:43 +0900

    Add markdown content parsing and blog post listing

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkHtml from 'remark-html';
+
+// ブログ記事ページ
+export default async function BlogPost({ params }) {
+  // contentディレクトリ内のマークダウンファイル一覧を取得
+  const postsDirectory = path.join(process.cwd(), 'content');
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  // URLのパラメータから該当するファイル名を取得 (今回は hello-world)
+  const { slug } = params;
+  const filePath = path.join(process.cwd(), 'content', `${slug}.md`);
+
+  // ファイルの中身を取得
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const { data, content } = matter(fileContents);
+  const title = data.title; // 記事のタイトル
+  const processedContent = await unified().use(remarkParse).use(remarkHtml).process(content);
+  const contentHtml = processedContent.toString(); // 記事の本文をHTMLに変換
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: contentHtml }}></div>
+    </div>
+  );
+}
