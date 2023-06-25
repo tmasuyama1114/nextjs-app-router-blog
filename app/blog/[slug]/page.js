efd55e1b7de365a133e4fcd9879f5f6bce56e942
tmasuyama1114 <tmasuyama1114@gmail.com>
2023-06-25 09:17:53 +0900

    Add blog post content styles and update page layout

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -4,6 +4,7 @@ import matter from 'gray-matter';
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkHtml from 'remark-html';
+import './content.css';
 
 // ブログ記事ページ
 export default async function BlogPost({ params }) {
@@ -19,9 +20,16 @@ export default async function BlogPost({ params }) {
   const contentHtml = processedContent.toString(); // 記事の本文をHTMLに変換
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: contentHtml }}></div>
+    <div className="bg-white px-6 py-32 lg:px-8">
+      <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          {title}
+        </h1>
+        <div
+          className="mt-6"
+          dangerouslySetInnerHTML={{ __html: contentHtml }}
+        ></div>
+      </div>
     </div>
   );
 }
--- content/hello-world.md
+++ content/hello-world.md
@@ -4,8 +4,6 @@ date: "2023-06-24"
 description: "This is my first blog post!"
 ---
 
-# 見出し1
-
-## 見出し2
+## 見出し
 
 This is my first blog post!
